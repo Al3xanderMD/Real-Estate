2a001c61759c51268b9ed23418cc08d41675cfcb
Cochior Daniel <100221622+CochiorDaniel@users.noreply.github.com>
2023-11-05 21:09:19 +0200

    Commercial si dependentele + tabelele individuale

---
+++ Real_Estate/RealEstate.Domain/Entities/Partitioning.cs
@@ -0,0 +1,24 @@
+﻿using RealEstate.Domain.Common;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Partitioning
+    {
+        public Guid IdPartitioning { get; private set; }
+        public string Type { get; private set; }
+
+        public Partitioning(string type)
+        {
+            IdPartitioning = Guid.NewGuid();
+            Type = type;
+        }
+
+        public static Result<Partitioning> CreatePartitioning(string type)
+        {
+            if (string.IsNullOrWhiteSpace(type))
+                return Result<Partitioning>.Failure("'Type' must not be empty");
+
+            return Result<Partitioning>.Success(new Partitioning(type));
+        }
+    }
+}
