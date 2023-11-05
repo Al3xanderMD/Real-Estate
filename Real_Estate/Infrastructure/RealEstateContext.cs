1130ff5012ed3fd156d0a406bf7761463ecfb57b
Ionascu Alexandrin <125979825+Al3xanderMD@users.noreply.github.com>
2023-11-05 19:29:47 +0200

    Project Structure

--- Real_Estate/Infrastructure/RealEstateContext.cs
+++ Real_Estate/Infrastructure/RealEstateContext.cs
@@ -1,7 +1,16 @@
-﻿namespace Infrastructure
-{
-    public class RealEstateContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace Infrastructure
+{
+    public class RealEstateContext : DbContext
+    {
+        //public dbset<event> events { get; set; }
+        //public dbset<category> categories { get; set; }
+        //public dbset<order> orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=RealEstate;User Id=postgres;Password=root;");
+        }
     }
 }
---
+++ Real_Estate/Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Application.Contracts;
+using RealEstate.Domain.Common;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        private readonly RealEstateContext context;
+
+        public BaseRepository(RealEstateContext context)
+        {
+            this.context = context;
+        }
+        public virtual async Task<Result<T>> AddAsync(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+            await context.SaveChangesAsync();
+            return Result<T>.Success(entity);
+        }
+
+        public virtual async Task<Result<T>> DeleteAsync(Guid id)
+        {
+            var result = await FindByIdAsync(id);
+            if (result != null)
+            {
+                context.Set<T>().Remove(result.Value);
+                await context.SaveChangesAsync();
+                return Result<T>.Success(result.Value);
+            }
+            return Result<T>.Failure($"Entity with id {id} not found");
+        }
+
+        public virtual async Task<Result<T>> FindByIdAsync(Guid id)
+        {
+            var result = await context.Set<T>().FindAsync(id);
+            if (result == null)
+            {
+                return Result<T>.Failure($"Entity with id {id} not found");
+            }
+            return Result<T>.Success(result);
+        }
+
+        public virtual async Task<Result<IReadOnlyList<T>>> GetPagedReponseAsync(int page, int size)
+        {
+            var result = await context.Set<T>().Skip(page).Take(size).AsNoTracking().ToListAsync();
+            return Result<IReadOnlyList<T>>.Success(result);
+        }
+
+        public virtual async Task<Result<T>> UpdateAsync(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Result<T>.Success(entity);
+        }
+    }
+}
