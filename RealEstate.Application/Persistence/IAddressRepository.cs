a7f05c9ad691d1358d695d431ba24d6691089929
andrei.habas <habasescuandrei1234@gmail.com>
2023-11-07 23:17:23 +0200

    Fluent validation for Address and Agent

---
+++ RealEstate.Application/Persistence/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Application.Persistence
+{
+    internal interface IAddressRepository
+    {
+    }
+}
