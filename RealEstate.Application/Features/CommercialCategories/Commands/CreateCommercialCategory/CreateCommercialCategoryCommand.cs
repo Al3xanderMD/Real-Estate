16d770068da0c739c3331266db0e4cbd2976ee5a
Ionascu Alexandrin <125979825+Al3xanderMD@users.noreply.github.com>
2023-11-15 22:37:41 +0200

    Revert "Andrei"

--- RealEstate.Application/Features/CommercialCategories/Commands/CreateCommercialCategory/CreateCommercialCategoryCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace RealEstate.Application.Features.CommercialCategories.Commands.CreateCommercialCategory
-{
-    public class CreateCommercialCategoryCommand : IRequest<CreateCommercialCategoryCommandResponse>
-    {
-        public string CategoryName { get; set; } = default!;
-    }
-}
--- RealEstate.Application/Persistence/ICommercialCategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using RealEstate.Application.Contracts;
-using RealEstate.Domain.Entities;
-
-namespace RealEstate.Application.Persistence
-{
-    public interface ICommercialCategoryRepository : IAsyncRepository<CommercialCategory>
-    {
-    }
-}
