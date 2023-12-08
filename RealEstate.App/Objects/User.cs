2f02b295795fc4f613fbb334933c18bd31c5d0b4
andrei.habas <habasescuandrei1234@gmail.com>
2023-12-08 16:44:03 +0200

    register connection

---
+++ RealEstate.App/Objects/User.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.App.Objects
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
