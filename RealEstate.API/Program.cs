656778fb4178ffcd31da43b58a626a4b6f43f3e0
iuliamoisa <iuliaem16@yahoo.com>
2023-12-12 14:02:05 +0200

    login - output in consola

--- RealEstate.API/Program.cs
+++ RealEstate.API/Program.cs
@@ -100,6 +100,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("Open");
+
+
 app.UseAuthorization();
 
 app.MapControllers();
--- RealEstate.App/Program.cs
+++ RealEstate.App/Program.cs
@@ -30,6 +30,6 @@ builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredServic
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddHttpClient<IAuthentificationService, AuthentificationService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7165/");
+    client.BaseAddress = new Uri("https://localhost:7190/"); // 7165
 });
 await builder.Build().RunAsync();
