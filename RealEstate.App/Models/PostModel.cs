2a882f0f4c666fb42d83d900d08554f8d5186558
andrei.habas <habasescuandrei1234@gmail.com>
2023-12-20 21:28:16 +0200

    added profile page

---
+++ RealEstate.App/Models/PostModel.cs
@@ -0,0 +1,33 @@
+﻿namespace RealEstate.App.Models
+{
+	public class PostModel
+	{
+		public string PropertyType { get; set; }
+		public bool OfferType { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Location { get; set; }
+		public string RoomCount { get; set; }
+		public string Floor { get; set; }
+		public int FloorCount { get; set; }
+		public int BuildYear { get; set; }
+		public float Area { get; set; }
+		public float UsefulSurface { get; set; }
+		public string LotType { get; set; }
+		public string Address { get; set; }
+		public float StreetFrontage { get; set; }
+		public float LotArea { get; set; }
+		public float Price { get; set; }
+		public List<string> ImageUrl { get; set; }
+		public string ContactEmail { get; set; }
+		public string ContactPhone { get; set; }
+		public string ContactName { get; set; }
+		public string UserId { get; set; }
+
+		public PostModel()
+		{
+			ImageUrl = new List<string>();
+			BuildYear = DateTime.Now.Year;
+		}
+	}
+}
---
+++ RealEstate.Identity/Migrations/20231219160834_CreatingDbUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstate.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingDbUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
