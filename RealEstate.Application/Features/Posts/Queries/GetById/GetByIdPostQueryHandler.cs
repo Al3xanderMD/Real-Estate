3c5df12f64aa8d2ef59d8b65bbe6ce5fd82951f2
Ionascu Alexandrin <125979825+Al3xanderMD@users.noreply.github.com>
2024-01-17 22:46:22 +0200

    GetById Posts Controllor

--- RealEstate.Application/Features/Posts/Queries/GetById/GetByIdPostQueryHandler.cs
+++ RealEstate.Application/Features/Posts/Queries/GetById/GetByIdPostQueryHandler.cs
@@ -6,19 +6,28 @@ namespace RealEstate.Application.Features.Posts.Queries.GetById
 	public class GetByIdPostQueryHandler : IRequestHandler<GetByIdPostQuery, PostsDto>
 	{
 		private readonly IPostRepository repository;
+		private readonly IBasePostRepository basePostRepository;
+		private readonly IAddressRepository addressRepository;
 
-		public GetByIdPostQueryHandler(IPostRepository repository)
+		public GetByIdPostQueryHandler(IPostRepository repository, IBasePostRepository basePostRepository, IAddressRepository addressRepository)
 		{
 			this.repository = repository;
+			this.basePostRepository = basePostRepository;
+			this.addressRepository = addressRepository;
 		}
 
 		public async Task<PostsDto> Handle(GetByIdPostQuery request, CancellationToken cancellationToken)
 		{
 			var post = await repository.FindByIntIdAsync(request.Id);
-			if (post.IsSuccess) 
+            var result2 = await basePostRepository.GetAllAsync();
+            var result3 = await addressRepository.GetAllAsync();
+
+
+            if (post.IsSuccess) 
 			{
 				return new PostsDto
 				{
+					Id = post.Value.Id,
 					BasePostId = post.Value.BasePostId,
 					BasePost = post.Value.BasePost,
 					Type = post.Value.Type
